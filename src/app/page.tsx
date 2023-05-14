248520796297ef65824b70a806445c39db93f600
Flawn <baran.peters@lessing-ffm.net>
2023-05-14 19:19:37 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Interactive from "./Interactive";
+import dynamic from "next/dynamic";
 
 export default function Home() {
-  return <Interactive aspectRatio={554 / 397} allowFullscreen={true} />;
+  return (
+    <Interactive aspectRatio={554 / 397} allowFullscreen={true}></Interactive>
+  );
 }
+
+const Interactive = dynamic(() => import("./Interactive"), {
+  ssr: false,
+});
